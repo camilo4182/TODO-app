da5aa26f935494dff06fd3dac2e2aae97575b720
Johan Camilo Cortes <camilo4182@gmail.com>
2020-04-27 18:34:19 -0500

    First commit.

---
+++ backend/backend/urls.py
@@ -0,0 +1,11 @@
+# backend/urls.py
+
+
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from todo import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
